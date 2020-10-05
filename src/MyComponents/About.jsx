c49e118cfa7dc01aad6da113e7fe7a8b8af21cb8
kazirafi71 <kazirafi66@gmail.com>
2020-10-05 10:26:35 +0600

    update

--- src/MyComponents/About.jsx
+++ src/MyComponents/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <div className="py-5" style={{backgroundColor:'#FF849C', fontFamily:'Quicksand', fontWeight:'600'}}>
             <h1 className='text-light m-5 p-4 '>About Us</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam eaque ab veniam at ipsam, asperiores cumque deleniti enim, autem dignissimos aut culpa debitis impedit soluta doloremque repellat? Quas, ducimus?</p>
+            
             </div>
         </div>
     );
